a063661383e601c23d439fd6e36c7fd081898506
Juanancg <juanan96lrs@gmail.com>
2018-05-14 19:45:25 +0200

    2º Commit - Funciona el if en el loop

--- MQTT_to_ESP32_Servo/MQTT_to_ESP32_Servo.ino
+++ MQTT_to_ESP32_Servo/MQTT_to_ESP32_Servo.ino
@@ -1,27 +1,25 @@
 #include <WiFi.h>
 
 #include <PubSubClient.h>
-
-const char* ssid = "yourNetworkName";
-
-const char* password =  "yourNetworkPassword";
-
-const char* mqttServer = "m11.cloudmqtt.com";
-
-const int mqttPort = 12948;
-
-const char* mqttUser = "yourMQTTuser";
-
-const char* mqttPassword = "yourMQTTpassword";
-
  
-//char* msg_received[7] = "Servo+90" o "Servo-90"
-WiFiClient espClient;
+/* WiFi */
+const char* ssid = "****************";
+const char* password =  "*************";
 
+/* MQTT - Utilizando CloudMQTT */
+const char* mqttServer = "*************";
+const int mqttPort = **********;
+const char* mqttUser = "**************";
+const char* mqttPassword = "**********";
+ 
+ 
+
+WiFiClient espClient;
 PubSubClient client(espClient);
 
+char msg_received[7]; // Solo debería recibir "Servo+90" o "Servo-90"
 
-void callback(char* topic, byte* payload, unsigned int length) { //Función que se llama cuando se un mensaje llega.   
+void callback(char* topic, byte* payload, unsigned int length) { //Función que es llamada cuando se un mensaje llega.   
 
   Serial.print("Message arrived in topic: ");
 
@@ -31,8 +29,7 @@ void callback(char* topic, byte* payload, unsigned int length) { //Función que 
 
   for (int i = 0; i < length; i++) {
 
-    Serial.print(msg_received[i]=((char)payload[i])); //Probar esto en Visual Studio 
-
+    Serial.print(msg_received[i]=((char)payload[i])); //It works!!
   }
   Serial.println();
   Serial.println("-----------------------");
@@ -79,16 +76,18 @@ void loop() {
 
   client.loop();
   
-  //Controlar el servo
-  
-  int giro=0; //Probablemente esta variable se tenga que poner fuera para no resetearla (Probar con un codigo simple de en el loop poner i++;
-  
-  if (msg_received="Servo+90"){
-  
-    for(giro = 0;giro<MAX_PWN;giro++){
-      
-      analog.write(giro); //NO ES ASÍ !! SUBIR EL CODIGO A GITHUB PARA TENERLO ACCESIBLE
-    } 
-}
+  if(msg_received[0]=='H'){ //Actualmente, si el ultimo mensaje empieza por H, se repite todo el rato.
+    Serial.print("He pasado!: ");
+    Serial.println(msg_received);
+  }
+//  int giro=0; //Probablemente esta variable se tenga que poner fuera para no resetearla (Probar con un codigo simple de en el loop poner i++;
+//  CAMBIAR TODO ESTO ->
+//  if (msg_received="Servo+90"){
+//  
+//    for(giro = 0;giro<MAX_PWN;giro++){
+//      
+//      analog.write(giro); //NO ES ASÍ !! SUBIR EL CODIGO A GITHUB PARA TENERLO ACCESIBLE
+//    } 
+//  }
 }
 
